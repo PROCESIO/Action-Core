efdd9e1dbc4cf67ebf94fa9dde046305300f46cb
cosminprocesio <cosmin.nastase@procesio.com>
2021-03-16 18:06:52 +0200

    Added ApiCredentials and new FE component.

---
+++ Ringhel.Procesio.Action.Core/Credentials/ApiCredentials.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ringhel.Procesio.Action.Core.Credentials
+{
+    public class ApiCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string BaseUrl { get; set; }
+        public string AuthenticationType { get; set; }
+        public string ApiKeyLocation { get; set; }
+    }
+}
