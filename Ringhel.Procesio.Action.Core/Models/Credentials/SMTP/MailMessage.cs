4be4f73ab6431aaaac3b8a4adbd07dd1bd496e32
cosminprocesio <cosmin.nastase@procesio.com>
2021-04-05 13:30:37 +0300

    Added a new class with all the informations for a mail message.

---
+++ Ringhel.Procesio.Action.Core/Models/Credentials/SMTP/MailMessage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ringhel.Procesio.Action.Core.Models.Credentials.SMTP
+{
+    public class MailMessage
+    {
+        /// <summary>
+        /// Sender email address.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Sender display name
+        /// </summary>
+        public string From { get; set; }
+
+        /// <summary>
+        /// Body message
+        /// </summary>
+        public string Body { get; set; }
+
+        /// <summary>
+        /// Subject message
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// To emails
+        /// </summary>
+        public IEnumerable<string> To { get; set; }
+
+        /// <summary>
+        /// Cc emails
+        /// </summary>
+        public IEnumerable<string> Cc { get; set; }
+
+        /// <summary>
+        /// Bcc emails
+        /// </summary>
+        public IEnumerable<string> Bcc { get; set; }
+
+        /// <summary>
+        /// Type of boyd html/text.
+        /// </summary>
+        public bool IsBodyHtml { get; set; }
+    }
+}
