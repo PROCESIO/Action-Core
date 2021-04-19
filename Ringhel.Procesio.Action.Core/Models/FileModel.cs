a941062dafc13021614922827735f1ff1ee5ae8e
Alexandru Gal <alexandru.gal@procesio.com>
2021-04-19 23:26:36 +0300

    Add FileModel Add File in FeComponentType Bump to 1.0.3.12

---
+++ Ringhel.Procesio.Action.Core/Models/FileModel.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace Ringhel.Procesio.Action.Core.Models
+{
+    public class FileModel
+    {
+        public Stream File { get; }
+        public string Name { get; }
+
+        public FileModel(Stream file, string name)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            using (Stream responseStream = file)
+            {
+                responseStream.CopyTo(memoryStream);
+            }
+
+            File = memoryStream;
+            Name = name;
+        }
+    }
+}
