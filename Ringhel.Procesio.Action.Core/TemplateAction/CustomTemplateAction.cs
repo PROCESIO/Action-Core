e9100e482dfafcf87d35816255e609ef0bcbb6ce
Alexandru Gal <alexandru.gal@procesio.com>
2021-05-06 18:29:49 +0300

    Add new examples on CustomTemplateAction - list of numbers - list of files - list of custom type

--- Ringhel.Procesio.Action.Core/TemplateAction/CustomTemplateAction.cs
+++ Ringhel.Procesio.Action.Core/TemplateAction/CustomTemplateAction.cs
@@ -9,7 +9,8 @@ using System.Threading.Tasks;
 namespace TestAction
 {
     [ClassDecorator(Name = "Custom Template Action", Shape = ActionShape.Circle, Description = "Custom Template Action for adding 2 values", Classification = Classification.cat1)]
-    [FEDecorator(Label = "Configuration Modal", Type = FeComponentType.Modal, Parent = "Config_Modal", Tab = "Input Tab")]
+    [FEDecorator(Label = "Configuration Modal", Type = FeComponentType.Modal, Parent = "Config_Modal", Tab = "Input Tab", RowId = 1)]
+    [FEDecorator(Label = "Configuration Modal", Type = FeComponentType.Side_pannel, Parent = "Side_Panel_Parent", Tab = "Input Tab", RowId = 2)]
     public class MyCustomAction : IAction
     {
 
@@ -44,31 +45,86 @@ namespace TestAction
         #endregion
 
         #region Properties
-        [FEDecorator(Label = "First number to add", Type = FeComponentType.Select, Tab = "Input Tab", Options = "ConfigP1OptionsList")]
+        [FEDecorator(Label = "First number to add", Type = FeComponentType.Select, Tab = "Input Tab", Options = "ConfigP1OptionsList", RowId = 3)]
         [BEDecorator(IOProperty = Direction.Input)]
         [Validator(IsRequired = false)]
         public int Input1 { get; set; }
 
-        [FEDecorator(Label = "Second number to add", Type = FeComponentType.Number, Tab = "Input Tab", DefaultValue = 0)]
+        [FEDecorator(Label = "Second number to add", Type = FeComponentType.Number, Tab = "Input Tab", DefaultValue = 0, RowId = 4)]
         [BEDecorator(IOProperty = Direction.Input)]
         [Validator(IsRequired = false)]
         public int Input2 { get; set; }
 
-        [FEDecorator(Label = "Result", Type = FeComponentType.Number, Tab = "Output Tab", DefaultValue = 1)]
+        [FEDecorator(Label = "Result", Type = FeComponentType.Number, Tab = "Output Tab", DefaultValue = 1, RowId = 1)]
         [BEDecorator(IOProperty = Direction.Output)]
         [Validator(IsRequired = false, Expects = ExpectedType.Number)]
         public int Output1 { get; set; }
 
-        [FEDecorator(Label = "FE Config property 2", Type = FeComponentType.Select, Parent = "Config_Modal", Options = "ConfigP2Options")]
+        [FEDecorator(Label = "FE Config property 2", Type = FeComponentType.Select, Parent = "Config_Modal", Options = "ConfigP2Options", RowId = 1)]
         [BEDecorator(IOProperty = Direction.Output)]
         [Validator(IsRequired = false, Expects = ExpectedType.String)]
         public string OutConfig2 { get; set; }
+
+        #region List of numbers
+        /// <summary>
+        /// Please use IEnumerable for List properties.
+        /// </summary>
+        [FEDecorator(Label = "Number list", Type = FeComponentType.Number, Tab = "Input Tab", RowId = 5)]
+        [BEDecorator(IOProperty = Direction.Input)]
+        [Validator(IsRequired = true, Expects = ExpectedType.Number)]
+        public IEnumerable<int> InputList { get; set; }
+
+        [FEDecorator(Label = "Sum of list", Type = FeComponentType.Number, Tab = "Output Tab", RowId = 2)]
+        [BEDecorator(IOProperty = Direction.Output)]
+        [Validator(IsRequired = false)]
+        public int OutputSum { get; set; }
+        #endregion
+
+        #region List of Files
+        /// <summary>
+        /// Please use IEnumerable for List properties.
+        /// Remove IEnumerable when a single file is required.
+        /// For output files, provide a valid MemoryStream in the FileModel.File object
+        /// </summary>
+        [FEDecorator(Label = "Map attachment", Type = FeComponentType.File, Parent = "Side_Panel_Parent", RowId = 1)]
+        [BEDecorator(IOProperty = Direction.Input)]
+        [Validator(IsRequired = false)]
+        public IEnumerable<FileModel> FileList { get; set; }
+        #endregion
+
+        #region List of DataTypes
+        ///// <summary>
+        ///// This is for example and commented to prevent adding Newtonsoft.Json nuget in Action.Core library
+        ///// </summary>
+        //[FEDecorator(Label = "Custom data list input", Type = FeComponentType.DataType, Tab = "Input Tab", RowId = 6)]
+        //[BEDecorator(IOProperty = Direction.Input)]
+        //[Validator(IsRequired = false)]
+        //public IEnumerable<JObject> CustomTypeList { get; set; }
+        #endregion
+
         #endregion
 
         #region Execute
         public async Task Execute()
         {
+            // Use of simple inputs
             Output1 = Input1 + Input2;
+
+            // Use of numeric list:
+            int result = 0;
+            foreach(var inputValue in InputList)
+            {
+                result += inputValue;
+            }
+            OutputSum = result;
+
+            // Use of files:
+            foreach(var file in FileList)
+            {
+                _ = file.Name; // this is the file name if required.
+                _ = file.File; // this is the file Stream which can be used to retrieve file content.
+            }
+
         }
         #endregion
     }
