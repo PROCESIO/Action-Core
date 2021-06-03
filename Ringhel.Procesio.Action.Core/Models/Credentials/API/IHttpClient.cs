4ba8664feb40fa8e1af2efbfe400b57763a68759
cosminprocesio <cosmin.nastase@procesio.com>
2021-06-03 11:43:23 +0300

    Refactored the ApiCredentialManager:   - Replaced HttpClient from with a custom interface IHttpClient. Added location for basic auth.

---
+++ Ringhel.Procesio.Action.Core/Models/Credentials/API/IHttpClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ringhel.Procesio.Action.Core.Models.Credentials.API
+{
+    public interface IHttpClient
+    {
+        Task<HttpResponseMessage> GetAsync(string endpoint, Dictionary<string, string> queryParameters, Dictionary<string, string> headerParameters);
+
+        Task<HttpResponseMessage> DeleteAsync(string endpoint, Dictionary<string, string> queryParameters, Dictionary<string, string> headerParameters);
+
+        Task<HttpResponseMessage> PutAsync(string endpoint, Dictionary<string, string> queryParameters, Dictionary<string, string> headerParameters, object body);
+
+        Task<HttpResponseMessage> PostAsync(string endpoint, Dictionary<string, string> queryParameters, Dictionary<string, string> headerParameters, object body);
+
+        Task<HttpResponseMessage> PatchAsync(string endpoint, Dictionary<string, string> queryParameters, Dictionary<string, string> headerParameters, object body);
+    }
+}
