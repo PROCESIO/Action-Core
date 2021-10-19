1bfcdb68cd5b8de09b4863a90854b55ec9d9a2d1
ana-musat <anamaria.musat@procesio.com>
2021-10-19 10:24:08 +0300

    Change key type from string to CertificateStorage object

---
+++ Ringhel.Procesio.Action.Core/Models/Credentials/FTP/CertificateStorage.cs
@@ -0,0 +1,8 @@
+﻿namespace Ringhel.Procesio.Action.Core.Models.Credentials.FTP
+{
+    public class CertificateStorage
+    {
+        public string FileContent { get; set; }
+        public string FilePath { get; set; }
+    }
+}
