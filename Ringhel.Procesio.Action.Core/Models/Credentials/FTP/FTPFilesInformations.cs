06560cd67cb962b3bc89e5f533a921c9bee9fbc1
ana-musat <anamaria.musat@procesio.com>
2021-10-20 09:15:30 +0300

    Remove Newtonsoft.Json library and JsonProperty

--- Ringhel.Procesio.Action.Core/Models/Credentials/FTP/FTPFilesInformations.cs
+++ Ringhel.Procesio.Action.Core/Models/Credentials/FTP/FTPFilesInformations.cs
@@ -1,19 +1,13 @@
-﻿using Newtonsoft.Json;
-
-namespace Ringhel.Procesio.Action.Core.Models.Credentials.FTP
+﻿namespace Ringhel.Procesio.Action.Core.Models.Credentials.FTP
 {
     public class FTPFilesInformations
     {
-        [JsonProperty("size")]
         public int Size { get; set; }
 
-        [JsonProperty("name")]
         public string Name { get; set; }
 
-        [JsonProperty("type")]
         public string Type { get; set; }
 
-        [JsonProperty("path")]
         public string Path { get; set; }
     }
 }
