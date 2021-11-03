faf9ddbe72ea01934bf2d2a40e3a60a7ac954ed1
Bujenita Andrei <andrei.bujenita@procesio.com>
2021-11-03 15:47:45 +0200

    Added RenameFileWithinFolder method and increased verison

--- Ringhel.Procesio.Action.Core/Models/Credentials/FTP/IFtpSftpClient.cs
+++ Ringhel.Procesio.Action.Core/Models/Credentials/FTP/IFtpSftpClient.cs
@@ -6,5 +6,6 @@ namespace Ringhel.Procesio.Action.Core.Models.Credentials.FTP
     public interface IFtpSftpClient
     {
         Task<List<FTPFilesInformations>> ListFileNamesWithinFolder(string directoryPath, bool isRecursive, string type);
+        Task<bool> RenameFileWithinFolder(string directoryPath, string oldName, string newName);
     }
 }
