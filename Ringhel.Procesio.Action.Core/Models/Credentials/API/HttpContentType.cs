a15037ae3ba1bdf25702ff942c7bd42f56eab351
Alexandru Gal <alexandru.gal@procesio.com>
2022-02-11 00:03:18 +0200

    Add HttpContentType enum

---
+++ Ringhel.Procesio.Action.Core/Models/Credentials/API/HttpContentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ringhel.Procesio.Action.Core.Models.Credentials.API
+{
+    public enum HttpContentType
+    {
+        None = 0,
+        Json = 1,
+        FormData = 2,
+        FormUrlEncoded = 3,
+        FileMultipart = 4
+    }
+}
