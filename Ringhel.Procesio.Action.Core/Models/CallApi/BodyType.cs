e286c1a0b89b28ec09382f22749d229dae2ea2a6
corina.rusu <corina.rusu@procesio.com>
2022-06-08 20:36:25 +0300

    added classes for call api models

---
+++ Ringhel.Procesio.Action.Core/Models/CallApi/BodyType.cs
@@ -0,0 +1,11 @@
+﻿namespace Ringhel.Procesio.Action.Core.Models.CallApi
+{
+    public enum BodyType
+    {
+        NONE = 0,
+        BINARY = 1,
+        FORM_DATA = 2,
+        FORM_URL_ENCODED = 3,
+        RAW = 4,
+    }
+}
