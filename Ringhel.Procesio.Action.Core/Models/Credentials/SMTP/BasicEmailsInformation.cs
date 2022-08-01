5dfeb547388f03f716cf34396de1b8a3cf5348ff
ana-musat <anamaria.musat@procesio.com>
2022-08-01 18:20:51 +0300

    Create Credentials_Smtp_Inbound FE Component type. Remove MailDirection from SMTPCredentialsConfiguration. Create EmailsInformation class to return the result of the read method for InboundEmail action.

---
+++ Ringhel.Procesio.Action.Core/Models/Credentials/SMTP/BasicEmailsInformation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ringhel.Procesio.Action.Core.Models.Credentials.SMTP
+{
+    public class BasicEmailInformation
+    {
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        public string To { get; set; }
+        public string Cc { get; set; }
+        public string Bcc { get; set; }
+        public string Sender { get; set; }
+        public string Body { get; set; }
+        public string Status { get; set; }
+        public int Size { get; set; }
+        public DateTime SendDate { get; set; }
+        public string ImportanceLevel { get; set; }
+    }
+}
