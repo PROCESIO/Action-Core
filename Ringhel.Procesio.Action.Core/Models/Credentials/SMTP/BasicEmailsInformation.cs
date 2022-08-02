7702aef159048e98e4b57bbbea2b93077c22c96b
ana-musat <anamaria.musat@procesio.com>
2022-08-02 14:17:29 +0300

    Add Id property for FileModel

--- Ringhel.Procesio.Action.Core/Models/Credentials/SMTP/BasicEmailsInformation.cs
+++ Ringhel.Procesio.Action.Core/Models/Credentials/SMTP/BasicEmailsInformation.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Ringhel.Procesio.Action.Core.Models.Credentials.SMTP
 {
     public class BasicEmailInformation
     {
         public int Id { get; set; }
+        public List<Guid?> FileIds { get; set; } = new List<Guid?>();
         public string Subject { get; set; }
         public string To { get; set; }
         public string Cc { get; set; }
--- Ringhel.Procesio.Action.Core/Models/FileModel.cs
+++ Ringhel.Procesio.Action.Core/Models/FileModel.cs
@@ -8,10 +8,11 @@ namespace Ringhel.Procesio.Action.Core.Models
         public Stream File { get; set; }
         public string Name { get; set; }
         public string Hash { get; set; }
+        public Guid? Id { get; set; }
 
         public FileModel() {}
 
-        public FileModel(Stream file, string name, string hash = null)
+        public FileModel(Stream file, string name, string hash = null, Guid? id = null)
         {
             var memoryStream = new MemoryStream();
             using (var responseStream = file)
@@ -23,12 +24,13 @@ namespace Ringhel.Procesio.Action.Core.Models
             File = memoryStream;
             Name = name;
             Hash = hash;
+            Id = id;
         }
 
         public FileModel Clone()
         {
             File.Seek(0, SeekOrigin.Begin);
-            return new FileModel(File, Name, Hash);
+            return new FileModel(File, Name, Hash, Id);
         }
     }
 }
