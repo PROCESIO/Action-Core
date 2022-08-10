956f27a1efa6ec242b53d21d619696ebcaf9db50
ana-musat <anamaria.musat@procesio.com>
2022-08-10 09:44:47 +0300

    Send to the ReadEmails method the processed list of filters.

--- Ringhel.Procesio.Action.Core/Models/Credentials/SMTP/ISmtpClient.cs
+++ Ringhel.Procesio.Action.Core/Models/Credentials/SMTP/ISmtpClient.cs
@@ -6,6 +6,6 @@ namespace Ringhel.Procesio.Action.Core.Models.Credentials.SMTP
     public interface ISmtpClient
     {
         public Task SendEmail(MailMessage mailMessage, List<FileModel> attachments);
-        public Task<EmailsInformation> ReadEmails(bool unread, bool hasAttachments, string filter);
+        public Task<EmailsInformation> ReadEmails(bool unread, bool hasAttachments, List<string> inputFilters);
     }
 }
