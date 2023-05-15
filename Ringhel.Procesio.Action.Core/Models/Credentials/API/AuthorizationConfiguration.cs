402087d3aba17530a19b10b44c9310c1ac2d10e7
corina.rusu <corina.rusu@procesio.com>
2023-05-15 18:42:06 +0300

    added token on authorization config

--- Ringhel.Procesio.Action.Core/Models/Credentials/API/AuthorizationConfiguration.cs
+++ Ringhel.Procesio.Action.Core/Models/Credentials/API/AuthorizationConfiguration.cs
@@ -1,19 +1,20 @@
-﻿namespace Ringhel.Procesio.Action.Core.Models.Credentials.API
+﻿namespace Ringhel.Procesio.Action.Core.Models.Credentials.API;
+
+public class AuthorizationConfiguration
 {
-    public class AuthorizationConfiguration 
-    {
-        public AuthorizationType Type { get; set; }
+    public AuthorizationType Type { get; set; }
 
-        public string UserName { get; set; }
+    public string UserName { get; set; }
 
-        public string Password { get; set; }
+    public string Password { get; set; }
 
-        public string ApiKey { get; set; }
+    public string ApiKey { get; set; }
 
-        public string ApiValue { get; set; }
+    public string ApiValue { get; set; }
 
-        public ClientApiKeyLocation ApiKeyLocation { get; set; }
+    public string Token { get; set; }
 
-        public ClientApiKeyLocation BasicAuthLocation { get; set; }
-    }
+    public ClientApiKeyLocation ApiKeyLocation { get; set; }
+
+    public ClientApiKeyLocation BasicAuthLocation { get; set; }
 }
