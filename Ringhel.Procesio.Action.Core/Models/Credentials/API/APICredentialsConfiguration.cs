7594d7b779cc62d134c45084ac2e1acca72681d9
corina.rusu <corina.rusu@procesio.com>
2024-02-12 19:29:24 +0200

    renamed APICredentialsConfiguration with correct meanings

--- Ringhel.Procesio.Action.Core/Models/Credentials/API/APICredentialsConfiguration.cs
+++ Ringhel.Procesio.Action.Core/Models/Credentials/API/APICredentialsConfiguration.cs
@@ -1,14 +1,14 @@
 ﻿namespace Ringhel.Procesio.Action.Core.Models.Credentials.API
 {
-    public class APICredentialsConfiguration 
+    public class APICredentialsConfiguration
     {
         public string Schema { get; set; }
 
+        public string Host { get; set; }
+
         public string Path { get; set; }
 
-        public string PrefixEndpoint { get; set; }
-
-        public int PortNumber { get; set; }
+        public int Port { get; set; }
 
         public AuthorizationConfiguration Authorization { get; set; }
     }
