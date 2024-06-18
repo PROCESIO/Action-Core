16d0ce155001787caeab60f69dbdc4de74fa1526
nicolae-irimia <nicolae.irimia@procesio.com>
2024-06-18 16:25:01 +0300

    bump to 1.25.0 & updated IDbClient (#95)

--- Ringhel.Procesio.Action.Core/Models/Credentials/DB/IDbClient.cs
+++ Ringhel.Procesio.Action.Core/Models/Credentials/DB/IDbClient.cs
@@ -9,6 +9,10 @@ namespace Ringhel.Procesio.Action.Core.Models
         /// Execute a query that will return a list
         /// </summary>
         Task<DataSet> ExecuteQuery(string dbQuery);
+        /// <summary>
+        /// Execute a query that will return a list in a specified amount of time
+        /// </summary>
+        Task<DataSet> ExecuteQuery(string dbQuery, int timeout);
 
         /// <summary>
         /// Execute a command that will return an int
@@ -16,5 +20,11 @@ namespace Ringhel.Procesio.Action.Core.Models
         /// </summary>
         /// <returns></returns>
         Task<int> ExecuteCommand(string dbQuery);
+        /// <summary>
+        /// Execute a command that will return an int in a specified amount of time
+        /// Examples: COUNT, INSERT, UPDATE, ...
+        /// </summary>
+        /// <returns></returns>
+        Task<int> ExecuteCommand(string dbQuery, int timeout);
     }
 }
