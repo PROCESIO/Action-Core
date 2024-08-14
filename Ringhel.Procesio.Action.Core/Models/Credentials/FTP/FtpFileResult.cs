da54ebdb76affa95505f51299c1ba10e2ee291b7
CostinGabriel <80429103+CostinGabriel@users.noreply.github.com>
2024-08-14 17:16:40 +0300

    task/PRC-4334: Add new model. Update Ftp client interface

---
+++ Ringhel.Procesio.Action.Core/Models/Credentials/FTP/FtpFileResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Ringhel.Procesio.Action.Core.Models;
+
+public class FtpFileResult
+{
+    public FileModel File { get; set; }
+    public bool IsError { get; set; }
+    public string Error { get; set; }
+}
--- Ringhel.Procesio.Action.Core/Models/Credentials/FTP/IFtpSftpClient.cs
+++ Ringhel.Procesio.Action.Core/Models/Credentials/FTP/IFtpSftpClient.cs
@@ -1,14 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace Ringhel.Procesio.Action.Core.Models.Credentials.FTP
+﻿namespace Ringhel.Procesio.Action.Core.Models.Credentials.FTP
 {
     public interface IFtpSftpClient
     {
         Task<List<FTPFilesInformations>> ListFileNamesWithinFolder(string directoryPath, bool isRecursive, string type);
         Task<bool> RenameFile(string folderPath, string currentName, string newName);
         Task<bool> DeleteFile(string folder, string fileName);
+        Task<List<FtpFileResult>> DeleteFiles(List<FTPFilesInformations> files);
         Task<FileModel> DownloadFile(string folder, string fileName);
+        Task<List<FtpFileResult>> DownloadFiles(List<FTPFilesInformations> files);
         Task<bool> UploadFile(FileModel file, string folder);
         Task<bool> CreateFolder(string folderPath, string folderName);
         Task<bool> DeleteFolder(string folderPath, string folderName);
