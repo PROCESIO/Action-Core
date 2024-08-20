8cddc0903973863faa13030c4776462aaa26729b
CostinGabriel <80429103+CostinGabriel@users.noreply.github.com>
2024-08-20 15:00:07 +0300

    task/PRC-3723: Add email with invalid attachments to action result

--- Ringhel.Procesio.Action.Core/Models/Credentials/SMTP/EmailsInformation.cs
+++ Ringhel.Procesio.Action.Core/Models/Credentials/SMTP/EmailsInformation.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections.Generic;
+﻿namespace Ringhel.Procesio.Action.Core.Models.Credentials.SMTP;
 
-namespace Ringhel.Procesio.Action.Core.Models.Credentials.SMTP
+public class EmailsInformation
 {
-    public class EmailsInformation
-    {
-        public List<BasicEmailInformation> BasicInformation { get; set; } = new List<BasicEmailInformation>();
-        public List<FileModel> Attachments { get; set; } = new List<FileModel>();
-    }
+    public List<BasicEmailInformation> BasicInformation { get; set; } = new List<BasicEmailInformation>();
+    public List<BasicErrorEmailInformation> BasicErrorInformation { get; set; } = new List<BasicErrorEmailInformation>();
+    public List<FileModel> Attachments { get; set; } = new List<FileModel>();
 }
