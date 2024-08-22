e2a55558e5fc834aeb13b8bb03ddb3c5f712a3a4
CostinGabriel <80429103+CostinGabriel@users.noreply.github.com>
2024-08-22 18:38:17 +0300

    task/PRC-4365: Add new datamodel FileContent

---
+++ Ringhel.Procesio.Action.Core/Models/FileContent.cs
@@ -0,0 +1,10 @@
+﻿namespace Ringhel.Procesio.Action.Core.Models;
+
+public class FileContent
+{
+    public string Name { get; set; }
+    public string MimeType { get; set; }
+    public string Content { get; set; }
+
+    public FileContent() {}
+}
