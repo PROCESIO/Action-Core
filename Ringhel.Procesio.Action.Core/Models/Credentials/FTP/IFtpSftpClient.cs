1766e368d7d185da2db397757e1f2d4f0b8412c4
Alexandru Gal <alexandru.gal@procesio.com>
2025-10-03 14:09:54 +0300

    update FtpSftpClient interface method parameters by adding the optional useDefaultBaseFolder = false

--- Ringhel.Procesio.Action.Core/Models/Credentials/FTP/IFtpSftpClient.cs
+++ Ringhel.Procesio.Action.Core/Models/Credentials/FTP/IFtpSftpClient.cs
@@ -2,14 +2,14 @@
 
 public interface IFtpSftpClient
 {
-    Task<List<FTPFilesInformations>> ListFileNamesWithinFolder(string directoryPath, bool isRecursive, string type);
-    Task<bool> RenameFile(string folderPath, string currentName, string newName);
-    Task<bool> DeleteFile(string folder, string fileName);
+    Task<List<FTPFilesInformations>> ListFileNamesWithinFolder(string directoryPath, bool isRecursive, string type, bool useDefaultBaseFolder = false);
+    Task<bool> RenameFile(string folderPath, string currentName, string newName, bool useDefaultBaseFolder = false);
+    Task<bool> DeleteFile(string folder, string fileName, bool useDefaultBaseFolder = false);
     Task<List<FtpFileResult>> DeleteFiles(List<FTPFilesInformations> files, int numberOfParallelRuns);
-    Task<FileModel> DownloadFile(string folder, string fileName);
+    Task<FileModel> DownloadFile(string folder, string fileName, bool useDefaultBaseFolder = false);
     Task<List<FtpFileResult>> DownloadFiles(List<FTPFilesInformations> files, int numberOfParallelRuns);
     Task<bool> UploadFile(FileModel file, string folder);
     Task<List<FtpFileResult>> UploadFiles(List<FileModel> files, string folder, int numberOfParallelRuns);
-    Task<bool> CreateFolder(string folderPath, string folderName);
-    Task<bool> DeleteFolder(string folderPath, string folderName);
+    Task<bool> CreateFolder(string folderPath, string folderName, bool useDefaultBaseFolder = false);
+    Task<bool> DeleteFolder(string folderPath, string folderName, bool useDefaultBaseFolder = false);
 }
